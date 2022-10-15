69391868e56b5f6e026a73a05ead743b18010318
LoganDooley <logan_dooley@brown.edu>
2022-10-15 15:56:53 -0400

    Added cooldown

--- Platycake_Project/Assets/Components/Scripts/WorldItem.cs
+++ Platycake_Project/Assets/Components/Scripts/WorldItem.cs
@@ -6,9 +6,10 @@ public class WorldItem : MonoBehaviour
 {
     private Inventory.Item item;
     private Inventory inventory;
+    private float cooldown;
 
     void OnTriggerEnter(Collider collider) {
-        if(collider.tag == "Player") {
+        if (collider.tag == "Player" && cooldown == 0) {
             if(this.inventory.AddItem(this.item)) {
                 Destroy(this.gameObject);
             }
@@ -26,12 +27,12 @@ public class WorldItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cooldown = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cooldown = Mathf.Max(0, cooldown - Time.deltaTime);
     }
 }
