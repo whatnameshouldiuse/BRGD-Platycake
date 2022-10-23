57fddc4ccc4b644e03d300abbb9d9926c1a5cff7
JonChenSD <52507937+JonChenSD@users.noreply.github.com>
2022-10-23 18:22:37 -0400

    Added NPCs, Animation handler
    Rename scene b1 to world scene

--- Platycake_Project/Assets/Components/Scripts/PlayerMovement.cs
+++ Platycake_Project/Assets/Components/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float speed = 500f;
     private Rigidbody rb;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,21 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector3(x, rb.velocity.y, z) * speed * Time.deltaTime;
+       
+       //animation handler
+        if (x != 0 || z !=0) {
+             animator.SetBool("isWalking",true);
+        }
+        else{
+            animator.SetBool("isWalking",false);
+        }
+        
+        if(Input.GetKeyDown("e")){
+            animator.SetBool("isThrowing",true);
+        }
+         if(Input.GetKeyUp("e")){
+            animator.SetBool("isThrowing",false);
+        }
         
     }
 }
